9e716d4415e50608a0419f6a266e61cc4d317b99
raqeeboz1 cloud <raqeeboz2020@gmail.com>
2023-06-11 19:10:18 +0000

    updated seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Mohammed Alhaijamy','mohd.abduraqeeb@gmail.com','alhaijamy' ,'Mock'),
-  ('Ahmed Alhaijamy', 'mohd.abduraqeeb@gmail.com','ahmed' ,'MOCK'),
+  ('Mohammed Alhaijamy','mohd.abduraqeeb@gmail.com','alhaijamy' ,'MOCk'),
+  ('Ahmed Alhaijamy', 'mohd.abduraqeeb+ahmed@gmail.com','ahmed' ,'MOCK'),
   ('Harry Potter', 'harry@alhaijamy.com','harry' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
